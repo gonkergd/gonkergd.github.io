b2d78528bd0c2032bd5b491d733c3d30dbfd85ab
gonkergd <59105483+gonkergd@users.noreply.github.com>
2026-06-01 08:45:13 +0800

    FINALLY removed wrong directory landing page
    and fixed etch a sketch landmine variable not doing anything

--- edge-a-sketch/eas.js
+++ edge-a-sketch/eas.js
@@ -12,7 +12,7 @@ function generateGrid(length){
             let a = Math.random() * 256;
             let b = Math.random() * 256;
             let c = Math.random() * 256;
-            if (landmine && !(pipeBombAtX == i && pipeBombAtY == j)){
+            if (!landmine || !(pipeBombAtX == i && pipeBombAtY == j)){
             content.addEventListener("mouseover", () => 
                 {
                     if (content.style.backgroundColor == ""){
