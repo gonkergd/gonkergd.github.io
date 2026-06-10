f927131c1317b256f6c58616dfb37237d3b32895
gonkergd <59105483+gonkergd@users.noreply.github.com>
2026-06-10 10:20:45 +0800

    Fixed Tie determination & completed interface
    took forevre

--- triple-t/script.js
+++ triple-t/script.js
@@ -71,12 +71,17 @@ let game = (function () {
         // console.log(winner);
         if (winner === "X") return "Player Wins";
         // computer turn
+        if (getAvailableGrids().length == 0) {
+            return "Tie";
+        } 
         let n = computerTurn();
         winner = determineWinner();
         console.log(toString());
         // console.log(winner);
-        if (winner === "O") return "Computer Wins";
-        if (gameBoard.every((n) => n != " ")) return "Tie";
+        if (winner === "O") return n + "Computer Wins";
+        if (getAvailableGrids().length == 0) {
+            return "Tie";
+        }
         return n;
     };
     /* 
@@ -88,7 +93,7 @@ let game = (function () {
         gameBoard[6] + "|" + gameBoard[7] + "|" + gameBoard[8];
     };
     /*
-        Returns the tic-tac-toe board in String format.
+        Returns the available empty grids.
     */
     let getAvailableGrids = () => {
         let emptyGrids = [];
@@ -110,8 +115,11 @@ let game = (function () {
     Emulates a game of tic-tac-toe on the website.
 */
 let userInterface = function (game) {
+    /* best code ever? */
     let body = document.querySelector("body");
-    let playButton = document.querySelector("button");
+    let playButton = document.querySelector(".playerTurn");
+    let restart = document.createElement("button");
+    restart.textContent = "Restart!";
     let grid0 = document.querySelector("#zero");
     let grid1 = document.querySelector("#one");
     let grid2 = document.querySelector("#two");
@@ -120,13 +128,26 @@ let userInterface = function (game) {
     let grid5 = document.querySelector("#five");
     let grid6 = document.querySelector("#six");
     let grid7 = document.querySelector("#seven");
-    let grid8 = document.querySelector("#eight"); /* best code ever? */
+    let grid8 = document.querySelector("#eight"); 
+    let header = document.querySelector(".header");
+    let score = document.querySelector(".score");
+    let playerScore = 0;
+    let computerScore = 0;
     let board = [grid0, grid1, grid2, grid3, grid4, grid5, grid6, grid7, grid8];
     let emptyGrids = [0, 1, 2, 3, 4, 5, 6, 7, 8];
     let number = 0;
+    let gameContinuing = true;
     let touched = false;
     playButton.addEventListener("click", () => {
         touched = true;
+    });
+    restart.addEventListener("click", () => {
+        touched = false;
+        clear();
+        gameContinuing = true;
+        play();
+        body.appendChild(playButton);
+        body.removeChild(restart);
     });
     /* 
         Clears the board.
@@ -138,47 +159,68 @@ let userInterface = function (game) {
            board[i].textContent = "";
            board[i].style.color = "";
        }
-   }
+   };
+    /*
+        @param {number} winner - The winner of the game, with 0 being the computer and 1 being the player
+        Updates the scoreboard on the document.
+    */
+   let scoreUpdate = (winner) => {
+        if (winner) {
+            playerScore++;
+        } else {
+            computerScore++;
+        }
+        score.textContent = playerScore + " - " + computerScore;
+   };
     /* 
         Initializes a game of tic-tac-toe on the website.
     */
     let play = () => {
         // loop X refreshes
-        if (!touched) {
-            if (board[number].textContent != "O") {
-                board[number].textContent = "";
+        if (gameContinuing) {
+            if (!touched) {
+                if (board[number].textContent != "O") {
+                    board[number].textContent = "";
+                }
+                number = emptyGrids[Math.floor(Math.random() * emptyGrids.length)];
+                // console.log(number);
+                // console.log(board[number]);
+                board[number].textContent = "X";
             }
-            number = emptyGrids[Math.floor(Math.random() * emptyGrids.length)];
-            // console.log(number);
-            // console.log(board[number]);
-            board[number].textContent = "X";
-        }
-        else {
-            body.removeChild(playButton);
-            board[number].textContent = "X";
-            board[number].style.color = "red";
-            emptyGrids.splice(emptyGrids.indexOf(number), 1);
-            let compNum = game.playGame(number);
-            if (compNum === "Player Wins") {
-                alert("You asserted your dominance. You are a sigma.");
-                clear();
-            } else if (compNum === "Computer Wins") {
-                alert("You have been replaced.");
-                clear();
-            } else if (compNum === "Tie") {
-                clear();
-            } else {
-                number = emptyGrids[Math.floor(Math.random() * emptyGrids.length)]; //refresh number
-                emptyGrids.splice(emptyGrids.indexOf(compNum), 1);
-                // console.log(emptyGrids);
-                board[compNum].textContent = "O";
+            else {
+                board[number].textContent = "X";
+                board[number].style.color = "red";
+                emptyGrids.splice(emptyGrids.indexOf(number), 1);
+                let compNum = game.playGame(number);
+                if (compNum === "Player Wins") {
+                    header.textContent = "You asserted your dominance. You are a sigma.";
+                    scoreUpdate(1);
+                    body.removeChild(playButton);
+                    body.appendChild(restart);
+                    gameContinuing = false;
+                } else if (typeof compNum === "string" && compNum.substring(1) === "Computer Wins") {
+                    board[Number(compNum.substring(0,1))].textContent = "O";
+                    header.textContent = "You have been replaced.";
+                    scoreUpdate(0);
+                    body.removeChild(playButton);
+                    body.appendChild(restart);
+                    gameContinuing = false;
+                } else if (compNum === "Tie") {
+                    header.textContent = "Tie!";
+                    body.removeChild(playButton);
+                    body.appendChild(restart);
+                    gameContinuing = false;
+                } else {
+                    number = emptyGrids[Math.floor(Math.random() * emptyGrids.length)]; //refresh number
+                    emptyGrids.splice(emptyGrids.indexOf(compNum), 1);
+                    // console.log(emptyGrids);
+                    board[compNum].textContent = "O";
+                }
+                touched = false;
             }
-            touched = false;
-            body.appendChild(playButton);
+            setTimeout(play, 200); 
         }
-        setTimeout(play, 200); 
     };
     return { play };
 };
-let interface = userInterface(game);
-interface.play();
+userInterface(game).play();
